fd283248e02cae62c93e490d5761d2a2f13f2800
Maryia Bialetskaya <mari.beletskaya.94@mail.ru>
2022-10-02 13:10:08 -0400

    add solution to task 2.Access an Array's Contents Using Bracket Notation.js

---
+++ 2.Access an Array's Contents Using Bracket Notation.js
@@ -0,0 +1,7 @@
+//2.Access an Array's Contents Using Bracket Notation.js
+
+//TASK: In order to complete this challenge, set the 2nd position (index 1) of myArray to anything you want, besides the letter b.
+
+let myArray = ["a", "b", "c", "d"];
+myArray[1] = "go for walk";
+console.log(myArray);
